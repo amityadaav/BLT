9ee0e9bb376c8f9cb2c1f7c15aa8510bd0f34e72
Amit yadaav <amityadaav707@gmail.com>
2026-05-21 22:06:45 +0530

    update

--- server/server.js
+++ server/server.js
@@ -137,8 +137,10 @@ app.use(helmet());
 ───────────────────────────────────────────── */
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
-    "https://bluelith.vercel.app",
+      "https://bluelith.in",
+      "https://www.bluelith.in",
+      "https://bluelith.vercel.app",
+      "http://localhost:5173",
   ],
 
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
